e081db069b7a03f906bdd3e70a884959bb2e6aad
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2025-02-26 23:26:30 +0900

    Use DFS Backtracking #1

--- 숫자 만들기/[Yeyeong]solution.py
+++ 숫자 만들기/[Yeyeong]solution.py
@@ -3,53 +3,42 @@ from itertools import permutations
 sys.stdin = open("input.txt", "r")
 
 
-def dfs(visited, arg_list):
-    global perm
-    if len(arg_list) == 0:
-        perm.append(visited)
-    else:
-        for i in range(len(arg_list)):
-            # visited.append(arg_list[i])
-            dfs(visited + [arg_list[i]], arg_list[:i] + arg_list[i + 1:])
+def dfs(idx, result):
+    global result_max
+    global result_min
+
+    if idx == N - 1:
+        result_max = max(result_max, result)
+        result_min = min(result_min, result)
+        return
+
+    for e in range(4):
+        if operators[e] > 0:
+            operators[e] -= 1
+            if e == 0:
+                new_result = result + numbers[idx + 1]
+            elif e == 1:
+                new_result = result - numbers[idx + 1]
+            elif e == 2:
+                new_result = result * numbers[idx + 1]
+            elif e == 3:
+                new_result = int(result / numbers[idx + 1])
+
+            dfs(idx + 1, new_result)
+            operators[e] += 1
 
 
 T = int(input())
 
 for t in range(1, T + 1):
     N = int(input())
-    operators_num = list(map(int, input().split()))
-    numbers = list(input().split())
-
-    operators = ''
-    operators_basic = ['+', '-', '*', '/']
-    for n in range(4):
-        operators += operators_basic[n] * operators_num[n]
-
-    operators_list = list(operators)
-    perm = []
-    dfs([], operators_list)
-    equations = [numbers[::-1] + p for p in perm]
+    operators = list(map(int, input().split()))
+    numbers = list(map(int, input().split()))
 
     result_max = -float('inf')
     result_min = float('inf')
-    for equation in equations:
-        stack = []
-        for e in equation:
-            if e.isdecimal():
-                stack.append(int(e))
-            else:
-                fst = stack.pop()
-                snd = stack.pop()
-                if e == '+':
-                    stack.append(fst + snd)
-                elif e == '-':
-                    stack.append(fst - snd)
-                elif e == '*':
-                    stack.append(fst * snd)
-                elif e == '/':
-                    stack.append(int(fst / snd))
-        result_max = max(result_max, sum(stack))
-        result_min = min(result_min, sum(stack))
+
+    dfs(0, numbers[0])
     print(f"#{t} {result_max - result_min}")
 
 
