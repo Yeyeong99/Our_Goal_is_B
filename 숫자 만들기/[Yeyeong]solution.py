3b06770bd9ef226757a52d931093a86bfaecb569
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2025-02-26 22:42:48 +0900

    Try dfs #1

--- 숫자 만들기/[Yeyeong]solution.py
+++ 숫자 만들기/[Yeyeong]solution.py
@@ -2,6 +2,17 @@ import sys
 from itertools import permutations
 sys.stdin = open("input.txt", "r")
 
+
+def dfs(visited, arg_list):
+    global perm
+    if len(arg_list) == 0:
+        perm.append(visited)
+    else:
+        for i in range(len(arg_list)):
+            # visited.append(arg_list[i])
+            dfs(visited + [arg_list[i]], arg_list[:i] + arg_list[i + 1:])
+
+
 T = int(input())
 
 for t in range(1, T + 1):
@@ -15,7 +26,8 @@ for t in range(1, T + 1):
         operators += operators_basic[n] * operators_num[n]
 
     operators_list = list(operators)
-    perm = list((map(list, list(set(permutations(operators_list, len(operators_list)))))))
+    perm = []
+    dfs([], operators_list)
     equations = [numbers[::-1] + p for p in perm]
 
     result_max = -float('inf')
