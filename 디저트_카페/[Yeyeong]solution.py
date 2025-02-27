f5bd253ae23f0ca7c45873c3185c0d1300ea94bc
Yeyeong99 <gongsam99@gmail.com>
2025-02-27 15:50:48 +0900

    Idea 2 #2

--- 디저트_카페/[Yeyeong]solution.py
+++ 디저트_카페/[Yeyeong]solution.py
@@ -5,18 +5,33 @@ dx = [1, 1, -1, -1]
 dy = [1, -1, -1, 1]
 
 
-def dfs(i, j, result):
+def dfs(i, j, idx, result, current_sum):
     global max_sum
-    if idx:
-        max_sum = max(max_sum, result)
-        return
+
+    if len(idx) >= 2:
+        if i != idx[0][0] and j != idx[0][1] and desserts[i][j] in result:
+            return
+
+        if i == idx[0][0] and j == idx[0][1]:
+            max_sum = max(max_sum, current_sum)
+            return
     else:
-        result += desserts[i][j]
         for k in range(4):
             nx = i + dx[k]
             ny = j + dy[k]
-            if 0 <= nx < N and 0 <= ny < N:
-                dfs(nx, ny, result)
+            if nx < 0 or nx >= N:
+                continue
+            if ny < 0 or ny >= N:
+                continue
+            if nx == idx[-1][0]:
+                continue
+            if ny == idx[-1][1]:
+                continue
+
+            current_sum += desserts[nx][ny]
+            idx += [[nx, ny]]
+            result += [desserts[nx][ny]]
+            dfs(nx, ny, idx, result, current_sum)
 
 
 T = int(input())
@@ -26,3 +41,8 @@ for t in range(1, T + 1):
     desserts = [list(map(int, input().split())) for _ in range(N)]
 
     max_sum = -float('inf')
+    for x in range(N):
+        for y in range(N):
+            dfs(x, y, [[x, y]], [desserts[x][y]], desserts[x][y])
+
+    print(max_sum)
