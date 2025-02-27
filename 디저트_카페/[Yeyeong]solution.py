7ff37d442d1a671a18741e15c172517c5d77c5ff
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2025-02-27 21:59:56 +0900

    Solving #2

--- 디저트_카페/[Yeyeong]solution.py
+++ 디저트_카페/[Yeyeong]solution.py
@@ -1,54 +1,45 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
-dx = [1, 1, -1, -1]
-dy = [1, -1, -1, 1]
 
-
-def dfs(i, j, direction, idx, result, current_sum):
+def dfs(i, j, direction, idx, visited, current_sum):
     global max_sum
     if direction == 4:
         if i == idx[0][0] and j == idx[0][1]:
             max_sum = max(max_sum, current_sum)
             return
     else:
+        if (i < 0 or N <= i) and (j < 0 or N <= j):
+            return
+
+        idx += [[i, j]]
+        visited += [desserts[i][j]]
+        current_sum += desserts[i][j]
+
         if direction == 0:
-            if 0 <= i + 1 < N and 0 <= j + 1 < N:
-                idx += [[i + 1, j + 1]]
-                result += [desserts[i + 1][j + 1]]
-                current_sum += desserts[i + 1][j + 1]
-                dfs(i + 1, j + 1, 0, idx, result, current_sum)
-            elif i + 1 >= N or j + 1 >= N:
-                dfs(i, j, 1, idx, result, current_sum)
+            nx = i + 1
+            ny = j + 1
         elif direction == 1:
-            if 0 <= i + 1 < N and 0 <= j - 1 < N:
-                idx += [[i + 1, j - 1]]
-                result += [desserts[i + 1][j - 1]]
-                current_sum += desserts[i + 1][j - 1]
-                dfs(i + 1, j - 1, 1, idx, result, current_sum)
-            elif i + 1 >= N or j - 1 < N:
-                dfs(i, j, 2, idx, result, current_sum)
-
+            nx = i + 1
+            ny = j - 1
         elif direction == 2:
-            if 0 <= i - 1 < N and 0 <= j - 1 < N:
-                idx += [[i - 1, j - 1]]
-                result += [desserts[i - 1][j - 1]]
-                current_sum += desserts[i - 1][j - 1]
-                dfs(i - 1, j + 1, 2, idx, result, current_sum)
-            elif i - 1 < 0 or j - 1 > N:
-                dfs(i, j, 3, idx, result, current_sum)
-
+            nx = i - 1
+            ny = j - 1
         elif direction == 3:
-            if 0 <= i - 1 < N and 0 <= j + 1 < N:
-                idx += [[i - 1, j + 1]]
-                result += [desserts[i - 1][j + 1]]
-                current_sum += desserts[i - 1][j + 1]
-                dfs(i - 1, j + 1, 3, idx, result, current_sum)
-            elif i - 1 < 0 or j - 1 > N:
-                dfs(i, j, 4, idx, result, current_sum)
+            nx = i - 1
+            ny = j + 1
+
+        # 이미 방문한 카페로 되돌아 가는 경우 (Fig. 5)
+        if nx != idx[0][0] and ny != idx[0][1] and [nx, ny] in idx:
+            return
+        # 좌표는 다르지만 같은 종류의 디저트를 파는 경우
+        if desserts[nx][ny] in visited:
+            return
+
+        dfs(nx, ny, idx, visited, current_sum)
 
         idx = []
-        result = []
+        visited = []
         current_sum = 0
 
 
@@ -61,6 +52,6 @@ for t in range(1, T + 1):
     max_sum = -float('inf')
     for x in range(N):
         for y in range(N):
-            dfs(x, y, 0, [[x, y]], [desserts[x][y]], desserts[x][y])
+            dfs(x, y, 0, [], [], 0)
 
     print(max_sum)
