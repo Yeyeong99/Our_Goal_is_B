d35cc034d299fca413b04e5c8d9c2528285cb8f8
Yeyeong99 <gongsam99@gmail.com>
2025-02-27 17:32:36 +0900

    Use direction #2

--- 디저트_카페/[Yeyeong]solution.py
+++ 디저트_카페/[Yeyeong]solution.py
@@ -5,31 +5,45 @@ dx = [1, 1, -1, -1]
 dy = [1, -1, -1, 1]
 
 
-def dfs(i, j, idx, result, current_sum):
+def dfs(i, j, direction, idx, result, current_sum):
     global max_sum
     if len(idx) >= 2:
         if i == idx[0][0] and j == idx[0][1]:
             max_sum = max(max_sum, current_sum)
             return
     else:
-        for k in range(4):
-            nx = i + dx[k]
-            ny = j + dy[k]
-            if nx < 0 or nx >= N:
-                continue
-            if ny < 0 or ny >= N:
-                continue
-            if nx == idx[-1][0]:
-                continue
-            if ny == idx[-1][1]:
-                continue
-            if nx != idx[0][0] and ny != idx[0][1] and desserts[nx][ny] in result:
-                continue
+        if direction == 0:
+            if 0 <= i + 1 < N and 0 <= j + 1 < N:
+                idx += [[i + 1, j + 1]]
+                result += [desserts[i + 1][j + 1]]
+                current_sum += desserts[i + 1][j + 1]
+                dfs(i + 1, j + 1, 0, idx, result, current_sum)
+                idx = []
+                result = []
+                current_sum = 0
+            elif j + 1 >= N:
+                idx += [[i + 1, j - 1]]
+                result += [desserts[i + 1][j - 1]]
+                current_sum += desserts[i + 1][j - 1]
+                dfs(i + 1, j - 1, 1, idx, result, current_sum)
+        elif direction == 1:
+            if 0 <= i + 1 < N and 0 <= j + 1 < N:
+                idx += [[i + 1, j + 1]]
+                result += [desserts[i + 1][j + 1]]
+                current_sum += desserts[i + 1][j + 1]
+                dfs(i + 1, j + 1, 0, idx, result, current_sum)
+                idx = []
+                result = []
+                current_sum = 0
+            elif j + 1 >= N:
+                idx += [[i + 1, j - 1]]
+                result += [desserts[i + 1][j - 1]]
+                current_sum += desserts[i + 1][j - 1]
+                dfs(i + 1, j - 1, 1, idx, result, current_sum)
 
-            current_sum += desserts[nx][ny]
-            idx += [[nx, ny]]
-            result += [desserts[nx][ny]]
-            dfs(nx, ny, idx, result, current_sum)
+        idx = []
+        result = []
+        current_sum = 0
 
 
 T = int(input())
@@ -41,6 +55,6 @@ for t in range(1, T + 1):
     max_sum = -float('inf')
     for x in range(N):
         for y in range(N):
-            dfs(x, y, [[x, y]], [desserts[x][y]], desserts[x][y])
+            dfs(x, y, 0, [[x, y]], [desserts[x][y]], desserts[x][y])
 
     print(max_sum)
