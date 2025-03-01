f06490f8908ba8bd2d357d268f5b73ebc59ffb88
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2025-03-02 02:31:19 +0900

    Solved Using Combination #4

--- 요리사/[Yeyeong]solution.py
+++ 요리사/[Yeyeong]solution.py
@@ -1,32 +1,6 @@
 import sys
+from itertools import combinations
 sys.stdin = open("input.txt", "r")
-
-
-def dfs(i, selected_idx, current_sum, total_sum):
-    global min_diff
-    if i > N:
-        return
-    # 두 개의 인덱스가 선택 됨
-    if len(selected_idx) == 2:
-        x = selected_idx[0]
-        y = selected_idx[1]
-        current_sum += [synergies[x][y] + synergies[y][x]]
-        selected_idx = []
-
-    # 식재료 절반이 선택 됨
-    if len(current_sum) == N // 2 and total_sum == 0:
-        total_sum = sum(current_sum)
-        current_sum = []
-
-    # 식재료가 모두 선택됨: 두 번째 합을 빼줌
-    elif len(current_sum) == N // 2 and total_sum != 0:
-        total_sum -= sum(current_sum)
-        min_diff = min(abs(total_sum), min_diff)
-        return
-    ingredient = ingredients[: i] + ingredients[i + 1:]
-    for j in ingredient:
-        dfs(j, selected_idx, current_sum, total_sum)
-
 
 T = int(input())
 
@@ -37,8 +11,18 @@ for t in range(1, T + 1):
 
     min_diff = float('inf')
 
-    for n in ingredients:
-        dfs(n, [], [], 0)
+    combs = list(combinations(ingredients, N // 2))
+    len_combs = len(combs)
+    for i in range(len_combs // 2):
+        first = combs[i]
+        second = combs[len_combs - i - 1]
 
-    print(t, min_diff)
+        first_comb = list(combinations(first, 2))
+        second_comb = list(combinations(second, 2))
+
+        first_flavor = sum([synergies[i][j] + synergies[j][i] for i, j in first_comb])
+        second_flavor = sum([synergies[i][j] + synergies[j][i] for i, j in second_comb])
+        min_diff = min(abs(first_flavor - second_flavor), min_diff)
+    print(f"#{t} {min_diff}")
+
 
