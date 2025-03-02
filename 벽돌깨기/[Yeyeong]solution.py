1654344724b959e534216bdbad7db46761f9972f
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2025-03-02 16:33:21 +0900

    Use DFS #5

--- 벽돌깨기/[Yeyeong]solution.py
+++ 벽돌깨기/[Yeyeong]solution.py
@@ -6,10 +6,13 @@ dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 
-def explosion(i, j, block, visited, total):
+def explosion(i, j, block, visited):
+    global total
     bomb = block[i][j]
+    block[i][j] = 0
+
     for d in range(4):
-        for b in range(bomb):
+        for b in range(1, bomb):
             nx = i + dx[d] * b
             ny = j + dy[d] * b
 
@@ -17,12 +20,12 @@ def explosion(i, j, block, visited, total):
                 if [nx, ny] not in visited:  # 방문 하지 않은 좌표
                     if block[nx][ny] >= 1:  # 벽돌이 1보다 큰 수인 경우 - 다른 벽돌 깨기
                         original = [nx, ny]
-                        total += 1  # 깨진 벽돌 수 저장
                         visited += [original]
-                        explosion(nx, ny, block, visited, total)
-                        blocks[original[0]][original[1]] = 0
-                        visited.pop()
+                        total += 1  # 깨진 벽돌 수 저장
+                        explosion(nx, ny, block, visited)
 
+
+def gravity(block, W, H):
     # 깨진 벽돌을 아래로 옮기는 작업
     for m in range(W):
         for n in range(1, H):
@@ -31,25 +34,25 @@ def explosion(i, j, block, visited, total):
                 block[m][n - 1] = value  # 현재 값을 아래로 옮기고
                 block[m][n] = 0  # 현재 값은 0으로 업데이트
 
-    return block, total, visited
 
-
-def dfs(i, j, block, cnt, total):
-    global max_blocks
+def dfs(i, j, block, cnt):
+    global max_blocks, total
     if cnt == N:
         max_blocks = max(max_blocks, total)
-        return
+        print(max_blocks)
+        cnt = 1
+        total = 0
     else:
         blocks_copy = [b[:] for b in block]
         current_sum = 0
 
-        if block[i][j] != 0:
-            block, total, visited = explosion(i, j, block, [], total)
+        if block[i][j] == 1:
+            block[i][j] = 0
+        elif block[i][j] > 1:
+            explosion(i, j, block, [])
 
-            cnt += 1
-            dfs(i, j, block, cnt, total)
-            cnt -= 1
-            block = blocks_copy
+        cnt += 1
+    block = blocks_copy
 
 
 T = int(input())
@@ -61,7 +64,9 @@ for t in range(1, T + 1):
 
     max_blocks = -float('inf')
 
-    counts = 1
+    counts = 0
+    total = 0
     for w in range(W - 1):
         for h in range(H - 1, -1, -1):
-            dfs(w, h, blocks, counts, 0)
+            dfs(w, h, blocks, counts)
+    print(max_blocks)
