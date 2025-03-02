cdb65162def91443dd325c79683bf16b239f9bb3
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2025-03-02 17:30:43 +0900

    Use Product and DFS #6

--- 벽돌깨기/[Yeyeong]solution.py
+++ 벽돌깨기/[Yeyeong]solution.py
@@ -1,4 +1,5 @@
 import sys
+from itertools import product
 
 sys.stdin = open("input.txt", "r")
 
@@ -6,10 +7,10 @@ dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 
-def explosion(i, j, block, visited):
-    global total
-    bomb = block[i][j]
-    block[i][j] = 0
+def explosion(i, j, visited):
+    global blocks
+    bomb = blocks[i][j]
+    blocks[i][j] = 0
 
     for d in range(4):
         for b in range(1, bomb):
@@ -18,41 +19,17 @@ def explosion(i, j, block, visited):
 
             if 0 <= nx < H and 0 <= ny < W:  # 델타 탐색이 범위 안에 존재
                 if [nx, ny] not in visited:  # 방문 하지 않은 좌표
-                    if block[nx][ny] >= 1:  # 벽돌이 1보다 큰 수인 경우 - 다른 벽돌 깨기
-                        original = [nx, ny]
-                        visited += [original]
-                        total += 1  # 깨진 벽돌 수 저장
-                        explosion(nx, ny, block, visited)
+                    if blocks[nx][ny] >= 1:  # 벽돌이 1보다 큰 수인 경우 - 다른 벽돌 깨기
+                        visited += [[nx, ny]]
+                        explosion(nx, ny, visited)
 
 
-def gravity(block, W, H):
-    # 깨진 벽돌을 아래로 옮기는 작업
-    for m in range(W):
-        for n in range(1, H):
-            if block[m][n - 1] == 0:  # 아래 있는 숫자가 0 이면
-                value = block[m][n]
-                block[m][n - 1] = value  # 현재 값을 아래로 옮기고
-                block[m][n] = 0  # 현재 값은 0으로 업데이트
-
-
-def dfs(i, j, block, cnt):
-    global max_blocks, total
-    if cnt == N:
-        max_blocks = max(max_blocks, total)
-        print(max_blocks)
-        cnt = 1
-        total = 0
-    else:
-        blocks_copy = [b[:] for b in block]
-        current_sum = 0
-
-        if block[i][j] == 1:
-            block[i][j] = 0
-        elif block[i][j] > 1:
-            explosion(i, j, block, [])
-
-        cnt += 1
-    block = blocks_copy
+def gravity():
+    global blocks
+    for h in range(H - 1):
+        for w in range(W):
+            if blocks[h][w] != 0 and blocks[h + 1][w] == 0:
+                blocks[h][w], blocks[h + 1][w] = blocks[h + 1][w], blocks[h][w]
 
 
 T = int(input())
@@ -60,13 +37,22 @@ T = int(input())
 for t in range(1, T + 1):
     N, W, H = map(int, input().split())
     blocks = [list(map(int, input().split())) for _ in range(H)]
-    blocks = list(map(list, zip(*blocks[::-1])))
+    blocks_copy = [w[:] for w in blocks]
+    possible_w = [list(range(W))] * N
 
-    max_blocks = -float('inf')
+    possible_ws = list(product(*possible_w))
 
-    counts = 0
-    total = 0
-    for w in range(W - 1):
-        for h in range(H - 1, -1, -1):
-            dfs(w, h, blocks, counts)
-    print(max_blocks)
+    min_blocks = float('inf')
+
+    for ws in possible_ws:
+        for h in range(H):
+            for w in ws:
+                if blocks[h][w] != 0:
+                    explosion(h, w, [])
+                    gravity()
+            current_block = sum([1 for r in range(H) for c in range(W) if blocks[r][c] != 0])
+            min_blocks = min(min_blocks, current_block)
+            blocks = blocks_copy
+
+    print(min_blocks)
+
