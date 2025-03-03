5198dee80d707be8fd2e15f80832e1ad87d0e649
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2025-03-03 23:24:37 +0900

    Use Combination

--- 벌꿀 채취/[Yeyeong]2115.py
+++ 벌꿀 채취/[Yeyeong]2115.py
@@ -10,6 +10,7 @@ for t in range(1, T + 1):
     N, M, C = map(int, input().split())
     honeys = [list(map(int, input().split())) for _ in range(N)]
 
+    # 첫 번째 꿀  선택
     firsts = []
     for n in range(N):
         for m in range(N - M + 1):
@@ -20,32 +21,35 @@ for t in range(1, T + 1):
                     fst_totals += list(combinations(first, k))
                 firsts += [[n, range(m, m + M), first, fst_totals]]
 
+    # 두 번째 꿀 선택
     total_sum = -float('inf')
     for i, js, first, first_total in firsts:
-        if N == M:
+        if M * 2 > N:  # 같은 행에서 선택 못하는 경우 - 다음 행으로 범위 조정
             start = i + 1
         else:
             start = i
         for n in range(start, N):
             for m in range(N - M + 1):
                 snd_totals = []
-                if N != M:
-                    if i != n and range(m, m + M) != js:
-                        second = honeys[n][m: m + M]
-                        if max(second) <= C:
-                            for k in range(1, M + 1):
-                                snd_totals += list(combinations(second, k))
-                else:
+                if M * 2 > N:  # 같은 행에서 채취 못할 때 다음 행에서 채취
                     second = honeys[n][m: m + M]
                     if max(second) <= C:
                         for k in range(1, M + 1):
                             snd_totals += list(combinations(second, k))
+                else:  # 같은 행에서 채취 가능할 때
+                    if i != n and range(m, m + M) != js:  # 첫 번째 꿀의 인덱스와 다를 경우
+                        second = honeys[n][m: m + M]
+                        if max(second) <= C:
+                            for k in range(1, M + 1):
+                                snd_totals += list(combinations(second, k))
+                # 첫 번째 꿀의 최대
                 fst_sum = -float('inf')
                 for fst in first_total:
                     if sum(fst) <= C:
                         current_sum = sum([num ** 2 for num in fst])
                         fst_sum = max(fst_sum, current_sum)
 
+                # 두 번째 꿀의 최대
                 snd_sum = -float('inf')
                 for snd in snd_totals:
                     if sum(snd) <= C:
