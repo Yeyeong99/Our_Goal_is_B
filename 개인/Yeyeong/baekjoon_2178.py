1b414d75f3882326ac501b11c87f5a8afe209aa2
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2025-03-08 20:14:26 +0900

    Solved with BFS #7

--- 개인/Yeyeong/baekjoon_2178.py
+++ 개인/Yeyeong/baekjoon_2178.py
@@ -1,32 +1,35 @@
+from collections import deque
+
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 
-def dfs(i, j, distance, visited):
-    global min_distance
-    if min_distance != float('inf') and distance >= min_distance:
-        return
-    if i == N - 1 and j == M - 1:
-        min_distance = min(min_distance, distance)
+def bfs():
+    queue = deque([(0, 0, 1)])  # 첫 번째 좌표, 거리
+    visited = [[False] * M for _ in range(N)]
+    visited[0][0] = True
 
-    for k in range(4):
-        nx = i + dx[k]
-        ny = j + dy[k]
+    while queue:
+        x, y, distance = queue.popleft()
 
-        if 0 <= nx < N and 0 <= ny < M:
-            if maze[nx][ny] == 1 and not visited[nx][ny]:
-                visited[nx][ny] = True
-                distance += 1
-                dfs(nx, ny, distance, visited)
-                distance -= 1
-                visited[nx][ny] = False
+        if x == N - 1 and y == M - 1:
+            return distance
+
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+
+            if (nx >= N or nx < 0) or (ny < 0 or ny >= M):
+                continue
+            if maze[nx][ny] == 0:
+                continue
+            if visited[nx][ny]:
+                continue
+
+            visited[nx][ny] = True
+            queue.append((nx, ny, distance + 1))
 
 
 N, M = map(int, input().split())
 maze = [list(map(int, list(input()))) for _ in range(N)]
 
-min_distance = float('inf')
-visited = [[False] * M for _ in range(N)]
-dfs(0, 0, 1, visited)
-
-print(min_distance)
+print(bfs())
