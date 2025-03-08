5ae512a145788d472dc8a53ee049329fe439f55a
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2025-03-08 19:54:41 +0900

    Solving with DFS #7

--- 개인/Yeyeong/baekjoon_2178.py
+++ 개인/Yeyeong/baekjoon_2178.py
@@ -4,6 +4,8 @@ dy = [0, 0, -1, 1]
 
 def dfs(i, j, distance, visited):
     global min_distance
+    if min_distance != float('inf') and distance >= min_distance:
+        return
     if i == N - 1 and j == M - 1:
         min_distance = min(min_distance, distance)
 
@@ -12,19 +14,19 @@ def dfs(i, j, distance, visited):
         ny = j + dy[k]
 
         if 0 <= nx < N and 0 <= ny < M:
-            if maze[nx][ny] == 1 and [nx, ny] not in visited:
-                visited += [[nx, ny]]
+            if maze[nx][ny] == 1 and not visited[nx][ny]:
+                visited[nx][ny] = True
                 distance += 1
                 dfs(nx, ny, distance, visited)
                 distance -= 1
-                visited.pop()
+                visited[nx][ny] = False
 
 
 N, M = map(int, input().split())
 maze = [list(map(int, list(input()))) for _ in range(N)]
 
 min_distance = float('inf')
-
-dfs(0, 0, 1, [[0, 0]])
+visited = [[False] * M for _ in range(N)]
+dfs(0, 0, 1, visited)
 
 print(min_distance)
