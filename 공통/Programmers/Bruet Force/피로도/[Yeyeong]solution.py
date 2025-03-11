745563eb6ff483d0103bdfb9f07fe51234f19c26
Yeyeong <67695343+Yeyeong99@users.noreply.github.com>
2025-03-11 17:20:58 +0900

    Solving with DFS #9

--- 공통/Programmers/Bruet Force/피로도/[Yeyeong]solution.py
+++ 공통/Programmers/Bruet Force/피로도/[Yeyeong]solution.py
@@ -1,32 +1,28 @@
 def solution(k, dungeons):
     total_list = []
+
     def dfs(n, cnt, dungeons, visited):
-        nonlocal total_list
         if len(visited) == n:
-            print(visited)
-            total_list += [visited]
-            print(total_list)
-        else:
-            for d in range(len(dungeons)):
-                if dungeons[d] not in visited:
-                    visited += [dungeons[d]]
-                    dungeons_left = dungeons[:d] + dungeons[d + 1:]
-                    cnt += 1
-                    dfs(n, cnt, dungeons_left, visited)
-                    cnt -= 1
-                    dungeons_left = dungeons
-                    visited.pop()
+            total_list.append(visited[:])  # 새로운 리스트를 추가해야 함
+            return
+
+        for d in range(len(dungeons)):
+            if dungeons[d] not in visited:
+                visited.append(dungeons[d])  # 기존 리스트를 수정하지 말고 새로운 리스트를 사용
+                dfs(n, cnt + 1, dungeons, visited)
+                visited.pop()  # 백트래킹
 
     dfs(len(dungeons), 0, dungeons, [])
-    print(total_list)
     max_num = -99999999
-#     numbers = 0
-#     for total in total_list:
-#         for i in range(len(dungeons) - 1):
-#             left = total[i][0] - total[i][1]
-#             if total[i + 1][0] < left:
-#                 numbers += 1
-#         max_num = max(numbers, max_num)
-    
-    
-    return max_num
+    numbers = 1
+    for total in total_list:
+        for i in range(len(dungeons) - 1):
+            left = k - total[i][1]
+            if total[i + 1][0] < left:
+                numbers += 1
+                initial = left
+        max_num = max(numbers, max_num)
+        numbers = 0
+        initial = k
+        
+    return max_num 
