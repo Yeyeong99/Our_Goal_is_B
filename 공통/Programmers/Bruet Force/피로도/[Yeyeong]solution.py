0779224996f33f0e1af4e1155f19c3cdcfed48f4
Yeyeong <67695343+Yeyeong99@users.noreply.github.com>
2025-03-11 17:38:55 +0900

    Solved with DFS #9

--- 공통/Programmers/Bruet Force/피로도/[Yeyeong]solution.py
+++ 공통/Programmers/Bruet Force/피로도/[Yeyeong]solution.py
@@ -1,28 +1,19 @@
 def solution(k, dungeons):
-    total_list = []
+    max_num = 0  # 최대로 돌 수 있는 던전 개수 저장
 
-    def dfs(n, cnt, dungeons, visited):
-        if len(visited) == n:
-            total_list.append(visited[:])  # 새로운 리스트를 추가해야 함
-            return
+    def dfs(current_k, dungeons, visited_count):
+        nonlocal max_num
+        max_num = max(max_num, visited_count)  # 최댓값 갱신
 
-        for d in range(len(dungeons)):
-            if dungeons[d] not in visited:
-                visited.append(dungeons[d])  # 기존 리스트를 수정하지 말고 새로운 리스트를 사용
-                dfs(n, cnt + 1, dungeons, visited)
-                visited.pop()  # 백트래킹
+        for i in range(len(dungeons)):
+            if dungeons[i] is None:  # 이미 방문한 던전 건너뜀
+                continue
+            min_req, consume = dungeons[i]
 
-    dfs(len(dungeons), 0, dungeons, [])
-    max_num = -99999999
-    numbers = 1
-    for total in total_list:
-        for i in range(len(dungeons) - 1):
-            left = k - total[i][1]
-            if total[i + 1][0] < left:
-                numbers += 1
-                initial = left
-        max_num = max(numbers, max_num)
-        numbers = 0
-        initial = k
-        
-    return max_num 
+            if current_k >= min_req:  # 던전을 돌 수 있는 경우만 진행
+                dungeons[i] = None  # 방문 처리
+                dfs(current_k - consume, dungeons, visited_count + 1)  # 체력 차감 후 다음 탐색
+                dungeons[i] = (min_req, consume)  # 원상 복구 (백트래킹)
+
+    dfs(k, dungeons, 0)
+    return max_num
