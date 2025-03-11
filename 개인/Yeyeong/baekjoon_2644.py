792a0ffd069c2abb7ffedb2a2aff2fd983fc679e
Yeyeong99 <gongsam99@gmail.com>
2025-03-12 08:17:49 +0900

    Solving with DFS #9

--- 개인/Yeyeong/baekjoon_2644.py
+++ 개인/Yeyeong/baekjoon_2644.py
@@ -7,14 +7,30 @@ M = int(input())
 family = {}
 for m in range(M):
     x, y = map(int, input().split())
-    if not family[x]:
+    if x not in family.keys():
         family[x] = []
+    if y not in family.keys():
+        family[y] = []
     family[x].append(y)
     family[y].append(x)
 
 answer = -1
 stack = [fst_person]
 cnt = 0
+visited = [0] * (N + 1)
 while stack:
     current = stack.pop()
-    cnt += 1
+    if current == snd_person:
+        visited[current] = 1
+
+        break
+
+    if family[current] and not visited[current]:
+        visited[current] = 1
+        stack += family[current][::-1]
+        cnt += 1
+    print(stack)
+if visited[snd_person]:
+    print(cnt)
+else:
+    print(answer)
