af960de96337463b8a77b8abb0711bbafaba4e0d
Yeyeong99 <gongsam99@gmail.com>
2025-05-07 12:14:33 +0900

    math.comb 사용

--- 개인/Yeyeong/baekjoon_1010.py
+++ 개인/Yeyeong/baekjoon_1010.py
@@ -1,14 +1,6 @@
+import math
+
 T = int(input())
-for t in range(T):
+for _ in range(T):
     n, m = map(int, input().split())
-
-    diff = m - n + 1
-    num = 1
-    result = 0
-
-    while diff > 0:
-        result += diff * num
-        diff -= 1
-        num += 1
-
-    print(result)
+    print(math.comb(m, n))
